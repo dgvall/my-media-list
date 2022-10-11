8c61a85e52516bcd2cad04f854a6ed485f96c840
Diego Valles <dgvall134@gmail.com>
2022-10-10 22:08:04 -0600

    Added pop-up menu

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ function search() {
   let form = document.getElementById("search-form")
   form.addEventListener("submit", (e) =>{
     e.preventDefault()
+    clearSearch()
     searchMovies(e.target.text.value)
   })
+}
+
+// Prevent searches from stacking
+function clearSearch() {
+  let container = document.getElementById("card-container")
+  container.innerHTML =  ``
 }
 
 // Get Request for API
@@ -25,10 +32,10 @@ function searchMovies(search) {
 
 // Create Cards
 function createCards(show) {
-  let container = document.getElementById("card-container")
-  let card = document.createElement("div")
-  let img = document.createElement("img")
-  let title = document.createElement("div")
+  const container = document.getElementById("card-container")
+  const card = document.createElement("div")
+  const img = document.createElement("img")
+  const title = document.createElement("div")
 
   card.className = "card"
   img.className = "cardImg"
@@ -39,7 +46,7 @@ function createCards(show) {
 
   // Create Forward Div on Click with more info
   card.addEventListener("click", (e) =>{
-    console.log(e)
+    showMore(show)
   })
 
 
@@ -60,5 +67,131 @@ function createCards(show) {
   container.appendChild(card)
   card.appendChild(img)
   card.appendChild(title)
+}
+
+function showMore(show) {
+  const window = document.createElement("div")
+  const overlay = document.createElement("div")
+  const header = document.createElement("div")
+  const title = document.createElement("div")
+  const button = document.createElement("button")
+  const body = document.createElement("div")
+  const status = document.createElement("div")
+  const score = document.createElement("div")
+  const episodes = document.createElement("div")
+  const foot = document.createElement("div")
+  const saveButton = document.createElement("button")
+
+
+  window.className = "pop-up"
+  overlay.id = "overlay"
+  header.className = "pop-up-header"
+  title.className = "pop-up-title"
+  button.className = "close-button"
+  body.className = "pop-up-body"
+  foot.className = "pop-up-foot"
+
+
+  title.textContent = show.name
+  button.innerHTML = "&times;"
+  saveButton.textContent = "Save"
+
+  button.addEventListener("click", (e) => removeElements(overlay, window))
+  overlay.addEventListener("click", (e) => removeElements(overlay, window))
+
+  // Pop up body
+
+
+  // Status
+  status.className = "pop-up-status"
+
+  const label = document.createElement("label")
+  const select = document.createElement("select")
+  const placeholder = document.createElement("option")
+  const planning = document.createElement("option")
+  const watching = document.createElement("option")
+  const completed = document.createElement("option")
+  const dropped = document.createElement("option")
+
+  label.className = "pop-up-labels"
+
+  label.textContent = "Status"
+  select.textContent = "Select"
+  placeholder.textContent = "Status"
+  placeholder.value = "none"
+  planning.textContent = "Plan to watch"
+  watching.textContent = "Watching"
+  completed.textContent = "Completed"
+  dropped.textContent = "Dropped"
+
+
+
+
+  // Score
+  const scoreLabel = document.createElement("label")
+  const emojis = document.createElement("div")
+  const smile = document.createElement("div")
+  const neutral = document.createElement("div")
+  const frown = document.createElement("div")
+
+  emojis.className = "emojis"
+  score.className = "pop-up-score"
+  scoreLabel.className = "pop-up-labels"
+  smile.className = "emoji"
+  neutral.className = "emoji"
+  frown.className = "emoji"
+
+  scoreLabel.textContent = "Score"
+  smile.innerText = String.fromCodePoint(0x1F642)
+  neutral.innerText = String.fromCodePoint(0x1F610)
+  frown.innerText = String.fromCodePoint(0x1F641)
+
+// Episodes
+  episodes.className = "pop-up-episodes"
+  const epLabel = document.createElement("label")
+  const epInput = document.createElement("input")
+
+  epLabel.className = "pop-up-labels"
+  epLabel.textContent = "Episodes Watched"
+  epInput.type = "text"
+  epInput.name = "text"
+
+  document.body.appendChild(window)
+  document.body.appendChild(overlay)
+  window.appendChild(header)
+  header.appendChild(title)
+  header.appendChild(button)
+  window.appendChild(body)
+  window.appendChild(foot)
+  foot.appendChild(saveButton)
+
+  //Status
+  body.appendChild(status)
+  status.appendChild(label)
+  status.appendChild(select)
+  select.appendChild(placeholder)
+  select.appendChild(planning)
+  select.appendChild(watching)
+  select.appendChild(completed)
+  select.appendChild(dropped)
+
+
+  // Score
+  body.appendChild(score)
+  score.appendChild(scoreLabel)
+  score.appendChild(emojis)
+  emojis.appendChild(smile)
+  emojis.appendChild(neutral)
+  emojis.appendChild(frown)
+
+  // Episodes
+  body.appendChild(episodes)
+  episodes.appendChild(epLabel)
+  episodes.appendChild(epInput)
+}
+
+function removeElements(element1, element2){
+  element1.remove()
+  element2.remove()
 }
 
